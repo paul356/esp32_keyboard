fbbc2fad912abecdeffc780c6832d991e3a71bc4
mattjin <jing.jin@zkh360.com>
2022-05-15 13:54:10 +0800

    feat: add key click event

--- web/app.js
+++ web/app.js
@@ -1,6 +1,5 @@
-import './common.scss'
-import './style.scss';
-
+import "./common.scss";
+import "./style.scss";
 
 const buttonOk = document.getElementById("button_ok");
 const buttonReset = document.getElementById("button_reset");
@@ -11,157 +10,169 @@ const row4 = document.getElementById("row_4");
 const row5 = document.getElementById("row_5");
 const row6 = document.getElementById("row_6");
 
-buttonOk.onclick = function(event) {
-  console.log(event);
-}
+// document.getElementById("app").addEventListener("click",function(event){
+//   var target = event.target;
+//   console.log(event);
+// });
 
-buttonReset.onclick = function(event) {
+buttonOk.onclick = function (event) {
   console.log(event);
-}
+};
 
-const elements1 = [
-  { item: 'Esc', width: 50 }
-]
-Array.from(Array(12).keys()).forEach(num => {
+buttonReset.onclick = function (event) {
+  console.log(event);
+};
+
+const elements1 = [{ item: "Esc", width: 50 }];
+Array.from(Array(12).keys()).forEach((num) => {
   elements1.push({
     item: `F${num + 1}`,
-    width: 50
+    width: 50,
   });
 });
 elements1.push({
-  item: 'Delete',
+  item: "Delete",
   width: 80,
   marginLeft: 20,
 });
 
 elements1.push({
-  item: 'Home',
+  item: "Home",
   width: 50,
   marginLeft: 40,
 });
 
-row1.innerHTML = elements1.map(item => {
-  return `<div class="key" style="width: ${item.width};${item.marginLeft&&('margin-left:'+item.marginLeft+'px')}">
+row1.innerHTML = elements1
+  .map((item) => {
+    return `<div class="key" style="width: ${item.width};${
+      item.marginLeft ? "margin-left:" + item.marginLeft + "px" : ""
+    }">
     <div class="inner">
-      <div class="align-center pl1 lh40">${item.item}</div>
+      <div class="align-center pl1 lh40" data-val="${item.item}">${
+      item.item
+    }</div>
     </div>
   </div>`;
-}).join('');
+  })
+  .join("");
 
 const elements2 = [
-  {top: '~', bottom: '`'},
-  {top: '!', bottom: '1'},
-  {top: '@', bottom: '2'},
-  {top: '#', bottom: '3'},
-  {top: '$', bottom: '4'},
-  {top: '%', bottom: '5'},
-  {top: '^', bottom: '6'},
-  {top: '&', bottom: '7'},
-  {top: '*', bottom: '8'},
-  {top: '(', bottom: '9'},
-  {top: ')', bottom: '0'},
-  {top: '_', bottom: '-'},
-  {top: '+', bottom: '='},
-  {name: "Backspace", width: 100},
-]
+  { top: "~", bottom: "`" },
+  { top: "!", bottom: "1" },
+  { top: "@", bottom: "2" },
+  { top: "#", bottom: "3" },
+  { top: "$", bottom: "4" },
+  { top: "%", bottom: "5" },
+  { top: "^", bottom: "6" },
+  { top: "&", bottom: "7" },
+  { top: "*", bottom: "8" },
+  { top: "(", bottom: "9" },
+  { top: ")", bottom: "0" },
+  { top: "_", bottom: "-" },
+  { top: "+", bottom: "=" },
+  { name: "Backspace", width: 100 },
+];
 
 elements2.push({
-  name: 'End',
+  name: "End",
   width: 50,
   marginLeft: 40,
 });
 
-row2.innerHTML = elements2.map(item => {
-  if (item.top) {
-    return `<div class="key">
+row2.innerHTML = elements2
+  .map((item) => {
+    if (item.top) {
+      return `<div class="key">
       <div class="inner flex-column space-between">
         <div class="f12 pl1">${item.top}</div>
         <div class="f12 pl1">${item.bottom}</div>
       </div>
     </div>`;
-  } else if (item.name === 'End') {
-    return `<div class="key" style="width: ${item.width};${item.marginLeft&&('margin-left:'+item.marginLeft+'px')}">
+    } else if (item.name === "End") {
+      return `<div class="key" style="width: ${item.width};${
+        item.marginLeft && "margin-left:" + item.marginLeft + "px"
+      }">
       <div class="inner">
         <div class="align-center pl1 lh40">${item.name}</div>
       </div>
-    </div>`
-  } else {
-    return `<div class="key" style="width: ${item.width}">
+    </div>`;
+    } else {
+      return `<div class="key" style="width: ${item.width}">
       <div class="inner">
-        <div class="${item.name==='Backspace'&&'lh40'}">${item.name}</div>
+        <div class="${item.name === "Backspace" && "lh40"}">${item.name}</div>
       </div>
     </div>`;
-  }
-}).join('');
+    }
+  })
+  .join("");
 
-const elements3 = [
-  {item: 'Tab', width: 75},
-];
+const elements3 = [{ item: "Tab", width: 75 }];
 
-['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'].forEach(char => {
+["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"].forEach((char) => {
   elements3.push({
     item: char,
     width: 50,
-  })
+  });
 });
 
 elements3.push({
-  item: ['{', '['],
+  item: ["{", "["],
   width: 50,
 });
 
 elements3.push({
-  item: ['}', ']'],
+  item: ["}", "]"],
   width: 50,
 });
 
 elements3.push({
-  item: ['|', '\\'],
+  item: ["|", "\\"],
   width: 75,
 });
 
 elements3.push({
-  item: 'Page Up',
+  item: "Page Up",
   width: 50,
   marginLeft: 40,
 });
 
-row3.innerHTML = elements3.map(item => {
-  if (Array.isArray(item.item)) {
-    return `<div class="key" style="width: ${item.width}">
+row3.innerHTML = elements3
+  .map((item) => {
+    if (Array.isArray(item.item)) {
+      return `<div class="key" style="width: ${item.width}">
       <div class="inner flex-column space-between">
         <div class="f12 pl1">${item.item[0]}</div>
         <div class="f12 pl1">${item.item[1]}</div>
       </div>
     </div>`;
-  } else {
-    return `<div class="key" style="width: ${item.width};${item.marginLeft&&('margin-left:'+item.marginLeft+'px')}">
+    } else {
+      return `<div class="key" style="width: ${item.width};${
+        item.marginLeft && "margin-left:" + item.marginLeft + "px"
+      }">
       <div class="inner">
         <div>${item.item}</div>
       </div>
     </div>`;
-  }
-}).join('');
+    }
+  })
+  .join("");
 
+const elements4 = [{ item: "Caps Lock", width: 100 }];
 
-const elements4 = [
-  {item: 'Caps Lock', width: 100},
-];
-
-['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'].forEach(char => {
+["A", "S", "D", "F", "G", "H", "J", "K", "L"].forEach((char) => {
   elements4.push({
     item: char,
     width: 50,
-  })
+  });
 });
 
 elements4.push({
-  item: [':', ';'],
+  item: [":", ";"],
   width: 50,
 });
 
 elements4.push({
-  item: ['"', '\''],
+  item: ['"', "'"],
   width: 50,
 });
 
@@ -171,103 +182,123 @@ elements4.push({
 });
 
 elements4.push({
-  item: 'Page Down',
+  item: "Page Down",
   width: 50,
   marginLeft: 40,
 });
 
-row4.innerHTML = elements4.map(item => {
-  if (Array.isArray(item.item)) {
-    return `<div class="key" style="width: ${item.width}">
+row4.innerHTML = elements4
+  .map((item) => {
+    if (Array.isArray(item.item)) {
+      return `<div class="key" style="width: ${item.width}">
       <div class="inner flex-column space-between">
         <div class="f12 pl1">${item.item[0]}</div>
         <div class="f12 pl1">${item.item[1]}</div>
       </div>
     </div>`;
-  } else {
-    return `<div class="key" style="width: ${item.width};${item.marginLeft&&('margin-left:'+item.marginLeft+'px')}">
+    } else {
+      return `<div class="key" style="width: ${item.width};${
+        item.marginLeft && "margin-left:" + item.marginLeft + "px"
+      }">
       <div class="inner">
-        <div class="${item.item==='Enter'&&'lh40'}">${item.item}</div>
+        <div class="${item.item === "Enter" && "lh40"}">${item.item}</div>
       </div>
     </div>`;
-  }
-}).join('');
+    }
+  })
+  .join("");
 
+const elements5 = [{ item: "Shift", width: 125 }];
 
-const elements5 = [
-  {item: 'Shift', width: 125},
-];
-
-['Z', 'X', 'C', 'V', 'B', 'N', 'M'].forEach(char => {
+["Z", "X", "C", "V", "B", "N", "M"].forEach((char) => {
   elements5.push({
     item: char,
     width: 50,
-  })
+  });
 });
 
 elements5.push({
-  item: ['<', ','],
+  item: ["<", ","],
   width: 50,
 });
 
 elements5.push({
-  item: ['>', '.'],
+  item: [">", "."],
   width: 50,
 });
 
 elements5.push({
-  item: ['?', '/'],
+  item: ["?", "/"],
   width: 50,
 });
 
-elements5.push({item: 'Shift', width: 95});
+elements5.push({ item: "Shift", width: 95 });
 
-elements5.push({item: '&#8593;', width: 50, align: 'm'});
+elements5.push({ item: "&#8593;", width: 50, align: "m" });
 
-row5.innerHTML = elements5.map(item => {
-  if (Array.isArray(item.item)) {
-    return `<div class="key" style="width: ${item.width}">
+row5.innerHTML = elements5
+  .map((item) => {
+    if (Array.isArray(item.item)) {
+      return `<div class="key" style="width: ${item.width}">
       <div class="inner flex-column space-between">
         <div class="f12 pl1">${item.item[0]}</div>
         <div class="f12 pl1">${item.item[1]}</div>
       </div>
     </div>`;
-  } else {
-    return `<div class="key" style="width: ${item.width}">
-      <div class="inner ${item.align==='m'&&'justify-center'}">
-        <div class="${(item.item==='Shift' || item.align==='m')&&'lh40'}">${item.item}</div>
+    } else {
+      return `<div class="key" style="width: ${item.width}">
+      <div class="inner ${item.align === "m" && "justify-center"}">
+        <div class="${
+          (item.item === "Shift" || item.align === "m") && "lh40"
+        }">${item.item}</div>
       </div>
     </div>`;
-  }
-}).join('');
-
+    }
+  })
+  .join("");
 
 const elements6 = [
-  {name: "Ctrl", width: 66},
-  {name: "Win", width: 66},
-  {name: "Alt", width: 66},
-  {name: "", width: 312},
-  {name: "Alt", width: 66},
-  {name: "Fn", width: 50},
-  {name: "Ctrl", width: 66},
-  {name: "&#8592;", width: 50, align: 'm'},
-  {name: "&#8595;", width: 50, align: 'm'},
-  {name: "&#8594;", width: 50, align: 'm'},
-]
+  { name: "Ctrl", width: 66 },
+  { name: "Win", width: 66 },
+  { name: "Alt", width: 66 },
+  { name: "", width: 312 },
+  { name: "Alt", width: 66 },
+  { name: "Fn", width: 50 },
+  { name: "Ctrl", width: 66 },
+  { name: "&#8592;", width: 50, align: "m" },
+  { name: "&#8595;", width: 50, align: "m" },
+  { name: "&#8594;", width: 50, align: "m" },
+];
 
-row6.innerHTML = elements6.map(item => {
-  if (item.top) {
-    return `<div class="key">
+row6.innerHTML = elements6
+  .map((item) => {
+    if (item.top) {
+      return `<div class="key">
       <div class="inner flex-column space-between">
         <div class="f12 pl1">${item.top}</div>
         <div class="f12 pl1">${item.bottom}</div>
       </div>
     </div>`;
-  } else {
-    return `<div class="key" style="width: ${item.width}">
-      <div class="inner ${item.align==='m'&&'justify-center'}">
+    } else {
+      return `<div class="key" style="width: ${item.width}">
+      <div class="inner ${item.align === "m" && "justify-center"}">
         <div class="lh40">${item.name}</div>
       </div>
     </div>`;
+    }
+  })
+  .join("");
+
+const keyBtn = document.getElementsByClassName("key");
+
+for (var i = 0; i < keyBtn.length; i++) {
+  keyBtn[i].addEventListener("click", handleClick, false);
+}
+
+function handleClick(event) {
+  for (var i = 0; i < keyBtn.length; i++) {
+    keyBtn[i].classList.remove("key-active");
   }
-}).join('');
+  event.currentTarget.classList.add("key-active");
+  console.log(event.currentTarget);
+}
