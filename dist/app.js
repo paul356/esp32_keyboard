37b02e4d13fabbb789fe0c376ba64797b67d34b6
paul356 <panhao356@gmail.com>
2023-03-06 14:31:08 +0800

    Display the keymap layout in web

--- dist/app.js
+++ dist/app.js
@@ -42,20 +42,56 @@ function upload() {
     }
 }
 
+function _create_element(tag, content, attrs) {
+    var div = document.createElement(tag);
+    for (var attr in attrs) {
+        div.setAttribute(attr, attrs[attr]);
+    }
+    if (content != null) {
+        div.innerHTML = content;
+    }
+    return div;
+}
+
+function _create_div(content, attrs) {
+    return _create_element("div", content, attrs);
+}
+
+function _render_keymap(layout_div, name, keymap) {
+    layout_div.append(_create_div(name, {"class" : "title"}));
+    
+    var render_key = function(key, index, arr) {
+        layout_div.append(_create_div(key, {"class" : "keystroke"}));
+    }
+
+    var render_row = function(row, index, arr) {
+        // render row here
+        row.forEach(render_key);
+        layout_div.append(_create_element("br", null, {}));
+    };
+    keymap.forEach(render_row);
+}
+
 function render_layouts() {
     var layouts_path = "/api/layouts";
 
-    var xhhtp = new XMLHttpRequest();
+    var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (xhttp.readyState == 4) {
-            var layout_div = document.getElementById("key_layouts");
-            var layout_json = JSON.parse(xhttp.responseText);
-            var keymap = layout_json["layouts"];
-            for (var layer in keymap) {
-                // continue here
+            if (xhttp.status == 200) {
+                var layout_div = document.getElementById("key_layouts");
+                var layout_json = JSON.parse(xhttp.responseText);
+                var keymap = layout_json["layouts"];
+                for (var layer in keymap) {
+                    _render_keymap(layout_div, layer, keymap[layer]);
+                }
+            } else {
+                _handle_server_error(xhttp);
             }
-        } else {
-            _handle_server_error(xhttp);
         }
     }
+
+    xhttp.open("GET", layouts_path, true);
+    xhttp.send();
 }
+
