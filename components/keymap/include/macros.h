8b7b45ec90facb616cbe2b9211c366cba687433a
Paul356 <panhao356@gmail.com>
2023-12-08 19:18:44 +0800

    add forgotten macros.h/c files

--- components/keymap/include/macros.h
+++ components/keymap/include/macros.h
@@ -8,6 +8,7 @@
 #define MACRO_CODE_MIN SAFE_RANGE
 #define MACRO_CODE_MAX (MACRO_CODE_MIN + MACRO_CODE_NUM - 1)
 
+#define MACRO_NAME_PREFIX "MACRO"
 #define MACRO_STR_MAX_LEN 1024
 
 esp_err_t set_macro_str(uint16_t keycode, char* buf);
