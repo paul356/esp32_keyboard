31ef035b32f9c6b253cbbca8fce63494ec8eb5f7
Paul356 <panhao356@gmail.com>
2023-12-08 17:08:21 +0800

    add the missing head file macros.h

---
+++ components/keymap/include/macros.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <stdbool.h>
+#include "esp_err.h"
+#include "quantum_keycodes.h"
+
+#define MACRO_CODE_NUM 32
+#define MACRO_CODE_MIN SAFE_RANGE
+#define MACRO_CODE_MAX (MACRO_CODE_MIN + MACRO_CODE_NUM - 1)
+
+#define MACRO_STR_MAX_LEN 1024
+
+esp_err_t set_macro_str(uint16_t keycode, char* buf);
+esp_err_t get_macro_str(uint16_t keycode, char* buf, int buf_len);
