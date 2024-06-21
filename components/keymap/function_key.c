01c8dc567fc023efba3397e175c4179ab1edb192
paul356 <panhao356@gmail.com>
2024-06-21 11:39:03 +0800

    Fix code license (#32)
    * update GPL license
    
    * add license declaration to code

--- components/keymap/function_key.c
+++ components/keymap/function_key.c
@@ -1,3 +1,23 @@
+/*
+ *  This program is a keyboard firmware for ESP family boards inspired by
+ *  MK32 and qmk_firmware.
+ *
+ *  Copyright (C) 2024 github.com/paul356
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <string.h>
 #include "function_key.h"
 #include "quantum.h"
